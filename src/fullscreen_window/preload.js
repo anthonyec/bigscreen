382c4188c6b45b73646e0fdcd0333cc64cddda67
Anthony Cossins <anthonyecossins@gmail.com>
2017-05-04 16:11:31 +0100

    Added comment description explaining preload scripts

--- src/fullscreen_window/preload.js
+++ src/fullscreen_window/preload.js
@@ -1,3 +1,7 @@
+/*
+ * This will be loaded before other scripts run in the web page.
+ * Preload scripts have access to node.js and electron APIs.
+ */
 const electronSettings = require('electron-settings');
 
 // Merge window globals with the globals inside config.yaml.
