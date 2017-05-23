71a2127a9bea5c276f9c991b8a7f6b987cbb498f
Anthony Cossins <anthonyecossins@gmail.com>
2017-05-23 11:53:58 +0100

    Removed boilerplate

--- renderer_process/src/preferences_screen/index.jsx
+++ renderer_process/src/preferences_screen/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 export function PreferencesScreen() {
   return (
@@ -8,13 +7,3 @@ export function PreferencesScreen() {
     </div>
   );
 }
-
-PreferencesScreen.propTypes = {};
-
-export function mapStateToProps() { // state
-  return {};
-}
-
-export const PreferencesScreenContainer = connect(
-  mapStateToProps
-)(PreferencesScreen);
