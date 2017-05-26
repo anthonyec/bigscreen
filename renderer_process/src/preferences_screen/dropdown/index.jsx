c6fd463271a38f2bf4d28af03e5617bbb8a29e22
Anthony Cossins <anthonyecossins@gmail.com>
2017-05-26 10:09:47 +0100

    Added dropdown component

---
+++ renderer_process/src/preferences_screen/dropdown/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import classes from './dropdown.css';
+import downArrow from './down_arrow.svg';
+
+export class Dropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({ value: evt.target.value });
+  }
+
+  render() {
+    return (
+      <div className={ classes.dropdown }>
+        <span className={ classes.label }>
+          { this.state.value || this.props.defaultValue }
+        </span>
+        <img className={ classes.arrow } src={ downArrow }/>
+        <select
+          ref="select"
+          className={ classes.select }
+          onChange={ this.handleChange }
+          defaultValue={ this.props.defaultValue }>
+          { this.props.children }
+        </select>
+        <div className={ classes.focusRing } />
+      </div>
+    );
+  }
+}
+
+Dropdown.propTypes = {
+  value: React.PropTypes.string,
+  defaultValue: React.PropTypes.string,
+  children: React.PropTypes.oneOfType([
+    React.PropTypes.array,
+    React.PropTypes.object,
+  ]),
+};
